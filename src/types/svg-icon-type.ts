127854e86f74bfc1e62e4b37e39a394fd46c6f71
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-04-03 14:24:43 +0100

    refactor(svg-icon-types): update svg icon name types with newly added svg icons id

--- src/types/svg-icon-type.ts
+++ src/types/svg-icon-type.ts
@@ -1,1 +1,1 @@
-export type ISvgIconNameTypes = 'create' | 'notification' | 'customize' | 'bell' | 'hamburger-menu' | 'close'
+export type ISvgIconNameTypes = 'create' | 'notification' | 'customize' | 'bell' | 'hamburger-menu' | 'close' | 'template' | 'download' | 'instagram' | 'linkedin' | 'facebook';
