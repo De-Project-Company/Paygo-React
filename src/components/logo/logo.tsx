35f825d8bc385a8454d93bbe7b119245c1076ae3
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-04-03 14:28:17 +0100

    refactor(logo): update logo componenent to have secondary prop

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 import logoStyles from './logo.module.css';
 
-const Logo = () => {
+interface ILogoProps {
+   secondary?: boolean;
+}
+
+const Logo = ({ secondary }: ILogoProps) => {
    return (
       <Link to='/' className={logoStyles.logo}>
-         <img src="/assets/logo.png" alt="PayGo logo" />
+         <img src={secondary ? "/assets/logo-white.png" : "/assets/logo.png"} alt="PayGo logo" />
       </Link>
    )
 }
