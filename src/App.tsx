adcfb945cf2b1d9389cedfb49bcdc6f1d36c07e3
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-05-27 15:40:45 +0100

    feat: wrap app in react query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesFiles from './components/Routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 // import Link from "./components/link";
 
 const App = () => {
+  const queryClient = new QueryClient()
   return (
+    <QueryClientProvider client={queryClient}>
     <Router>
       <RoutesFiles />
-    </Router>
+      </Router>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
