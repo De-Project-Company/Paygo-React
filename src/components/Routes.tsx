6046d9256559468464a58d45a20e4c3b38a883a1
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-05-27 15:39:23 +0100

    refactor(Router File): update router paths to use newly created route file

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -5,16 +5,17 @@ import Projects from "../pages/projects";
 import Signin from "../pages/Signin";
 import SignUp from "../pages/SignUp";
 import ClientPage from "../pages/ClientPage";
+import { routes } from "../lib/routes";
 
 const RoutesFiles = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/client" element={<ClientPage />} />
+      <Route path={routes.home()} element={<Home />} />
+      <Route path={routes.dashboard()} element={<Dashboard />} />
+      <Route path={routes.projects()} element={<Projects />} />
+      <Route path={routes.signIn()} element={<Signin />} />
+      <Route path={routes.signUp()} element={<SignUp />} />
+      <Route path={routes.clients()} element={<ClientPage />} />
     </Routes>
   );
 };
