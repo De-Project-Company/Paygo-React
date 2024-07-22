5029a58f0f7c95350e7783f443aa7748aa192934
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-07-22 17:55:38 +0100

    feat: create verification success page

---
+++ src/pages/verification-success.tsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+
+import { routes } from '@/lib/routes';
+import AuthLayout from '../components/auth-layout';
+import common from '@/styles/common.module.css';
+
+const VerificationSuccessPage = () => {
+  return (
+    <AuthLayout>
+      <div className="p-10 max-w-[1000px] bg-white mx-auto rounded-xl">
+        <div className="mx-auto  text-center w-max">
+          <div className="w-full max-w-[300px] mx-auto aspect-square ">
+            <img
+              src="/assets/verified.png"
+              alt="sign up mockup"
+              className="w-full"
+            />
+          </div>
+
+          <p className="py-5 font-bold">Congratulations</p>
+
+          <p className="font-normal">
+            Your account has been verified. you can now set up your account
+          </p>
+
+          <Link
+            to={routes.signIn()}
+            replace
+            className={`${common.btn} my-5 block`}
+          >
+            Set up your account
+          </Link>
+        </div>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default VerificationSuccessPage;
