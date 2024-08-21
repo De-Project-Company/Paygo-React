92f12a511133a3ed9b2a08f0fa0edfdfe4cded40
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-08-21 15:36:39 +0100

    refactor: update routes file to use dashboard layout for dashboard routes

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,24 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
+
 import Home from '../pages';
 import Dashboard from '../pages/dashboard';
-import Projects from '../pages/projects';
+import Invoices from '../pages/invoices';
 import Signin from '../pages/Signin';
 import SignUp from '../pages/SignUp';
 import ClientPage from '../pages/ClientPage';
-import { routes } from '../lib/routes';
 import VerificationSuccessPage from '../pages/verification-success';
+import DashboardLayout from './dashboard-layout';
+import Settings from '@/pages/settings';
 
-// TODO: Changes made here
+import { routes } from '../lib/routes';
+
 const RoutesFiles = () => {
   return (
     <Routes>
       <Route path={routes.home()} element={<Home />} />
-      <Route path={routes.dashboard()} element={<Dashboard />} />
-      <Route path={routes.projects()} element={<Projects />} />
       <Route path={routes.signIn()} element={<Signin />} />
       <Route path={routes.signUp()} element={<SignUp />} />
       <Route path={routes.verified()} element={<VerificationSuccessPage />} />
-      <Route path={routes.clients()} element={<ClientPage />} />
+
+      <Route element={<DashboardLayout />}>
+        <Route path={routes.dashboard()} element={<Dashboard />} />
+        <Route path={routes.dashboardInvoice()} element={<Invoices />} />
+        <Route path={routes.dashboardClients()} element={<ClientPage />} />
+        <Route path={routes.dashboardSettings()} element={<Settings />} />
+      </Route>
     </Routes>
   );
 };
